e964b2f8e495ab9c417e0212d17c01200307938b
evtishin <evtishin@gmail.com>
2023-11-08 19:54:05 +0000

    modified:   bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,13 +1,30 @@
-section .text
+ section .text
     global _start
 
 _start:
-    ; Your bootloader code goes here
+    mov ah, 0x0E    ; BIOS teletype mode
+    mov al, 'E'
+    int 0x10        ; BIOS video service
+    mov al, 'T'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 'a'
+    int 0x10
+    mov al, 'd'
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'r'
+       ; Repeat for each character...
 
     ; Hang the system
     hang:
         jmp hang
 
-    times 510 - ($ - $$) db 0
+    times 510-($-$$) db 0
     dw 0xAA55
-    
